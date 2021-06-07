5adb194fc5e1ab41f46fc0139e920d0c0bcf8fa1
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-07 20:02:48 +0530

    add initial content

---
+++ script.js
@@ -0,0 +1,8 @@
+const setOfWords=[
+    "The ability of science and technology to improve human life is known to us",
+    "Not that one had anything to do with the other, but the technology of surrogacy would have been inconceivable back then",
+    "Technology brings about economic wealth through improved production, facilitation of trade, and promoting the division of labor.",
+    "This work entitles Beckmann to be regarded as the founder of scientific technology, a term which he was the first to use in 1772.",
+    "We only have people doing this work because we have not yet developed the technology to get machines to do it."
+
+];
